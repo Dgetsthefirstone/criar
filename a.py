0fa379d3859789c985fe3c5bde6772564e3b8fae
Dgetsthefirstone <dgetsgamer@gmail.com>
2025-07-04 11:15:59 -0300

    alteração no submódulo

--- a.py
+++ a.py
@@ -2,22 +2,22 @@ usuarios = ['marcos','ana','gabriel']
 while True:
     pergunta = input("gostaria de remover um dos usuário?\n")
     if pergunta.title() == "Sim":
-        print(f'escola um deles\n\t {usuarios}')
+        print(f'escolha um deles\n\t {usuarios}')
         
         
         pergunta2 = input("digite qual\n")
-        for value in usuarios:
-               if pergunta2.lower() == value:
-                    usuarios.remove(value)
-                    print('o usuário foi removido com sucesso')
-                    print(usuarios)
-                    continue
-               else:
-                    print(f"o usuário {pergunta2} que você está chamando não existe, tente novamente")
+        
+        if pergunta2.lower() in usuarios:
+            usuarios.remove(pergunta2)
+            print('o usuário foi removido com sucesso')
+            print(usuarios)
+            break
+        else:
+            print(f"o usuário que você está chamando não existe. Tente novamente")
                     
 
     
-    elif pergunta.title() == 'Não':
+    elif pergunta.title() == 'nao' or pergunta.title() == 'não':
         print("obrigado por entrar")
         break
-print('A cessão acabou')
+print('A sessão acabou')
